d862404deb1899f8279af6543dad88b6fb0b385b
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2024-01-19 22:31:09 +0200

    Added product display

--- test_task/core/models.py
+++ test_task/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+
+def validate_count(value):
+    if value < 0:
+        raise ValidationError("Count must not be negative!")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    decription = models.CharField(max_length=200)
+    count = models.IntegerField(validators=[validate_count])
+    delivery_date = models.DateField(auto_now_add=True)
+    expiry_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        self.clean_fields()
+        self.validate_expiry_date()
+        super().save(*args, **kwargs)
+
+    def validate_expiry_date(self):
+        if self.expiry_date <= self.delivery_date:
+            raise ValidationError("Expiry date must be greater than delivery date!")
+
--- test_task/core/views.py
+++ test_task/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+import core.models as md
+
+
+class ProductListView(ListView):
+    model = md.Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
